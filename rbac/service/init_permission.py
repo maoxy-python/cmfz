48c4c6090b8aa1a52db97f3b9adc22699db3ae55
MaWenChao0417 <m13598195773@163.com>
2019-08-08 23:10:58 +0800

    在init_permission中添加了关于获取菜单信息的操作，在获取权限的时候连同菜单信息一同获取，一起存入session

--- rbac/service/init_permission.py
+++ rbac/service/init_permission.py
@@ -10,15 +10,36 @@ def init_permission(user, request):
     """
 
     # 2. 权限信息的初始化
-    role_list = user.roles.filter(permissions__isnull=False).values('permissions__url', 'permissions__url').distinct()
+    # 获取菜单相关的信息   title  is_menu  icon
+    role_list = user.roles.filter(permissions__isnull=False).values('permissions__id',
+                                                                    'permissions__url',
+                                                                    'permissions__title',
+                                                                    'permissions__is_menu',
+                                                                    'permissions__icon',).distinct()
 
     # 获取权限中所有的url并且保存到列表中
     # permission_list = []
     # for item in role_list:
     #     permission_list.append(item['permissions__url'])
 
+    # 3. 权限+ 菜单信息的初始化
+    menu_list = []
+    permission_list = []
+    for item in role_list:
+        # 在一个循环中完成权限和菜单的获取，不再使用列表生成式
+        if item['permissions__is_menu']:
+            permission_list.append(item['permissions__url'])
+            temp = {
+                'title': item['permissions__title'],
+                'icon': item['permissions__icon'],
+                'url': item['permissions__url'],
+            }
+            menu_list.append(temp)
+
     # 列表生成式
-    permission_list = [item['permissions__url'] for item in role_list]
+    # permission_list = [item['permissions__url'] for item in role_list]
 
     # 将权限保存到session中
     request.session[settings.PERMISSION_SESSION_KEY] = permission_list
+    # 将菜单的权限保存到session中
+    request.session[settings.MENU_SESSION_KEY] = menu_list
